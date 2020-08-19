3793a36768069c423a66d2a8bd5168ccf8872437
GabrielIbujes <49683647+GabrielIbujes@users.noreply.github.com>
2020-08-18 19:52:57 -0500

    Add files via upload

---
+++ tweets_donald.py
@@ -0,0 +1,45 @@
+import pymongo
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+
+
+ckey = "6Zyv4XxVypDqHDpFoHwSTrMzX"
+csecret = "3J5TpltHtmEZGEw8RhRLABc3KQ2Quhjj2SVVykfw5zs02fjtpC"
+atoken = "153168970-C8H0rPCjztDmLQMrjtgOYSPIzjLMyegrtrAZQQrq"
+asecret = "WxWpMOMlghN1tVYZRFugRWTefM1SShLWVI4lL4oPWTAlO"
+
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        dictTweet = json.loads(data)
+        try:
+            dictTweet["_id"] = str(dictTweet['id'])
+            doc = mycol.insert_one(dictTweet)
+            print("SAVED" + str(doc) + "=>" + str(data))
+        except:
+            print("Already exists")
+            pass
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, listener())
+
+'''Mmongo'''
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017")
+mydb=myclient["bdd3"]
+mycol = mydb["bdd3"]
+
+'''===============LOCATIONS=============='''
+
+
+twitterStream.filter(track=['Donald Trump'])
+
---
+++ tweets_newyork.py
@@ -0,0 +1,47 @@
+
+import couchdb
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+
+
+###API ########################
+ckey = "6Zyv4XxVypDqHDpFoHwSTrMzX"
+csecret = "3J5TpltHtmEZGEw8RhRLABc3KQ2Quhjj2SVVykfw5zs02fjtpC"
+atoken = "153168970-C8H0rPCjztDmLQMrjtgOYSPIzjLMyegrtrAZQQrq"
+asecret = "WxWpMOMlghN1tVYZRFugRWTefM1SShLWVI4lL4oPWTAlO"
+#####################################
+
+class listener(StreamListener):
+    
+    def on_data(self, data):
+        dictTweet = json.loads(data)
+        try:
+            
+            dictTweet["_id"] = str(dictTweet['id'])
+            doc = db.save(dictTweet)
+            print ("SAVED" + str(doc) +"=>" + str(data))
+        except:
+            print ("Already exists")
+            pass
+        return True
+    
+    def on_error(self, status):
+        print (status)
+        
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, listener())
+
+'''========couchdb'=========='''
+server = couchdb.Server('http://admin:admin@localhost:5984/')  #('http://115.146.93.184:5984/')
+try:
+    db = server.create('bd1')
+except:
+    db = server['bd1']
+    
+'''===============LOCATIONS=============='''    
+
+twitterStream.filter(locations=[-74.25909,40.477399,-73.700181,40.916178])  
+#twitterStream.filter(track=['newyork'])
